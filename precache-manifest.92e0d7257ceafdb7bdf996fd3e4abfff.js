40cbf8ad941d88f19ed3fb2da4e4a06b4a335844
nothingis <thenothinis@gmail.com>
2019-10-29 08:02:03 +0900

    Updates

---
+++ precache-manifest.92e0d7257ceafdb7bdf996fd3e4abfff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f9437e3120e0c72ddb2ccb9262d021",
+    "url": "/RiichiMahjong/index.html"
+  },
+  {
+    "revision": "73403808efd1e6a4f3ed",
+    "url": "/RiichiMahjong/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "89f797d357d23e39add1",
+    "url": "/RiichiMahjong/static/js/2.a6e49ef8.chunk.js"
+  },
+  {
+    "revision": "73403808efd1e6a4f3ed",
+    "url": "/RiichiMahjong/static/js/main.1f1f6df8.chunk.js"
+  },
+  {
+    "revision": "3c07962b51ad495306a7",
+    "url": "/RiichiMahjong/static/js/runtime-main.762067a0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/RiichiMahjong/static/media/logo.25bf045c.svg"
+  }
+]);
